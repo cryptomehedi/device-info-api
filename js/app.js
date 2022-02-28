bcceef477029a17903c83c6080dee23b8d13c6fa
cryptomehedi <classicalmehedi@gmail.com>
2022-03-01 02:17:36 +0600

    api linked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+const phoneNameInput = document.querySelector('.phone-name-input');
 const searchButton = document.querySelector('#search-button');
+const phoneDetails = document.querySelector('.phone-details');
+const phoneName = document.querySelector('.phone-name');
+
+const searchPhone = () =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${phoneNameInput.value}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}

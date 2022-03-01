2d60cbe3ae4a9c8e61c885da37177a5c968c63e9
cryptomehedi <classicalmehedi@gmail.com>
2022-03-01 20:09:24 +0600

    spinner add

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ const searchButton = document.querySelector('#search-button');
 const phoneDetails = document.querySelector('#phone-details');
 const phoneContainer = document.querySelector('.phone-container');
 const warningMessage = document.querySelector('.warning-message');
+const spinnerShow = document.querySelector('#spinner-show');
 
 // *search validation & api data collect
 const searchPhone = () =>{
+    spinnerLoad ('block');
     if(!isNaN(searchField.value)){
             emptyValue()
             const div = document.createElement('div');
@@ -21,6 +23,7 @@ const searchPhone = () =>{
                 p.innerText = ` Your Search Field Is Empty`;
                 div.appendChild(p)
                 warningMessage.appendChild(div)
+                spinnerLoad ('none');
     }else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`
         emptyValue();
@@ -66,9 +69,9 @@ const displaySearchResult = phonesData =>{
             `;
             div.appendChild(p)
             phoneContainer.appendChild(div)
-            
         })
     }
+    spinnerLoad ('none');
 }
 // *API id collection & data process
 const showPhoneDetails = phoneSlug =>{
@@ -124,4 +127,19 @@ const emptyValue = () => {
     phoneDetails.textContent=''
     warningMessage.textContent=''
     phoneContainer.textContent=''
+}
+
+const spinnerLoad = (param) =>{
+    
+    spinnerShow.innerHTML = `
+    <p class="inline-flex  items-center font-semibold leading-6 text-sm shadow rounded-md transition ease-in-out duration-150 hover:bg-gray-600 hover:text-white bg-gray-200 text-black border rounded-lg p-2 m-2 ring  focus:ring-offset-2 focus:ring-offset-orange-500">
+        <!-- <i class="fas fa-spinner"></i> -->
+        <svg class="animate-spin -ml-1 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        Loading ...
+        </p>
+    `
+    spinnerShow.style.display = param
 }

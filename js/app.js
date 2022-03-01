ca159fad7f6597be6ac2499252b3cf84332e99c6
cryptomehedi <classicalmehedi@gmail.com>
2022-03-01 19:04:53 +0600

    error check

--- js/app.js
+++ js/app.js
@@ -56,8 +56,7 @@ const displaySearchResult = phonesData =>{
         const phones = phonesData.data
         phoneContainer.textContent= ''
         warningMessage.textContent=''
-        phones.forEach (phone =>{
-            console.log(phone);
+        phones.slice(0,20).forEach (phone =>{
             const div = document.createElement('div');
             div.classList.add ('bg-orange-200');
             div.classList.add ('m-8');
@@ -89,6 +88,7 @@ const showPhoneDetails = phoneSlug =>{
     fetch(url)
     .then(res => res.json())
     .then(data => displayDetails(data.data))
+    // .then(data => console.log(data.data))
 }
 
 
@@ -104,19 +104,27 @@ const displayDetails= data =>{
     p.classList.add('lg:grid-cols-2')
     p.innerHTML=`
     
-        <div class="w-1/2 flex justify-end">
-            <img class="px-2" src="${data.image}">
+        <div class="lg:w-100 sm:w-2/3 px-8 flex sm:justify-center lg:justify-end">
+            <img class="" src="${data.image}">
         </div>
         <div class=" bg-orange-200 p-4 rounded">
             <div class="bg-white rounded p-4">
                 Name: ${data.name}<br>
                 Brand: ${data.brand}<br>
-                Release Date: ${data.releaseDate}<br>
-                Chip Set: ${data.mainFeatures.chipSet}<br>
-                Display Size: ${data.mainFeatures.displaySize}<br>
-                Memory: ${data.mainFeatures.memory}<br>
-                Storage: ${data.mainFeatures.storage}<br>
-                Sensor: ${data.mainFeatures.sensors}
+                Release Date: ${data.releaseDate ? data.releaseDate : 'No Data Found'}<br>
+                Chip Set: ${data.mainFeatures.chipSet ? data.mainFeatures.chipSet: 'No Data Found'}<br>
+                Display Size: ${data.mainFeatures.displaySize ? data.mainFeatures.displaySize : 'No Data Found'}<br>
+                Memory: ${data.mainFeatures.memory ? data.mainFeatures.memory : 'No Data Found'}<br>
+                Storage: ${data.mainFeatures.storage ? data.mainFeatures.storage : 'No Data Found'}<br>
+                Sensor: ${data.mainFeatures.sensors ? data.mainFeatures.sensors : 'No Data Found'}<br>
+                <span class="text-xl">Other</span><br>
+                Bluetooth: ${data.others?.Bluetooth ? data.others?.Bluetooth : 'No Data Found'}<br>
+                GPS: ${data.others?.GPS ? data.others?.GPS: 'No Data Found'}<br>
+                NFC: ${data.others?.NFC ? data.others?.NFC : 'No Data Found'}<br>
+                Radio: ${data.others?.Radio ? data.others?.Radio: 'No Data Found'}<br>
+                USB: ${data.others?.USB ? data.others?.USB : 'No Data Found'}<br>
+                Radio: ${data.others?.WLAN ? data.others?.WLAN : 'No Data Found'}
+                
             </div>
         </div>
     `;
